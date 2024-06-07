e7c9419508d14d8413cd1703d95747f7a7768a5a
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-06-07 16:42:21 +0900

    分かりにくいコメントアウトを修正

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ int main() {
     Compare<float> compFloat;
     Compare<double> compDouble;
 
-    // Min関数の呼び出しと結果の表示
+    // 同じ型での計算結果表示
     std::cout << "Min(3, 5) int: " << compInt.Min(3, 5) << std::endl;
     std::cout << "Min(3.5f, 2.5f) float: " << compFloat.Min(3.5f, 2.5f) << std::endl;
     std::cout << "Min(7.8, 2.3) double: " << compDouble.Min(7.8, 2.3) << std::endl;
@@ -27,7 +27,7 @@ int main() {
     Compare<int> compIntDouble;
     Compare<float> compFloatDouble;
 
-    // 型変換を伴う比較と結果の表示
+    // 型変換での計算結果表示
     std::cout << "Min(3, 5.5f) int-float: " << compInt.Min(3, static_cast<int>(5.5f)) << std::endl;
     std::cout << "Min(3, 6.7) int-double: " << compInt.Min(3, static_cast<int>(6.7)) << std::endl;
     std::cout << "Min(5.5f, 6.7) float-double: " << compFloat.Min(5.5f, static_cast<float>(6.7)) << std::endl;
