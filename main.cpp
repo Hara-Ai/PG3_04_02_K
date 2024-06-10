60f0ae34a333982b5ee4834cb68cc9fc9420c609
GC1B_12_ハラ_アイ <147122028+Hara-Ai@users.noreply.github.com>
2024-06-10 13:41:34 +0900

    訂正

--- main.cpp
+++ main.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 
 // クラステンプレートを定義
-template <typename T>
-
-class Compare {
+template <typename T1, typename T2>
+class Compare 
+{
 public:
-    // Minメンバ関数の定義
-    T Min(T a, T b) {
+    auto Min(T1 a, T2 b) -> decltype(a < b ? a : b) 
+    {
         return (a < b) ? a : b;
     }
 };
 
-int main() { 
-    // 各型の組み合わせでCompareクラスのインスタンスを作成
-    Compare<int> compInt;
-    Compare<float> compFloat;
-    Compare<double> compDouble;
+int main() {
+    Compare<int, int> compInt;
+    Compare<float, float> compFloat;
+    Compare<double, double> compDouble;
+    Compare<int, float> compIntFloat;
+    Compare<int, double> compIntDouble;
+    Compare<float, double> compFloatDouble;
 
     // 同じ型での計算結果表示
     std::cout << "Min(3, 5) int: " << compInt.Min(3, 5) << std::endl;
     std::cout << "Min(3.5f, 2.5f) float: " << compFloat.Min(3.5f, 2.5f) << std::endl;
     std::cout << "Min(7.8, 2.3) double: " << compDouble.Min(7.8, 2.3) << std::endl;
 
-    // 型を混ぜた組み合わせのインスタンスを生成
-    Compare<int> compIntFloat;
-    Compare<int> compIntDouble;
-    Compare<float> compFloatDouble;
-
-    // 型変換での計算結果表示
-    std::cout << "Min(3, 5.5f) int-float: " << compInt.Min(3, static_cast<int>(5.5f)) << std::endl;
-    std::cout << "Min(3, 6.7) int-double: " << compInt.Min(3, static_cast<int>(6.7)) << std::endl;
-    std::cout << "Min(5.5f, 6.7) float-double: " << compFloat.Min(5.5f, static_cast<float>(6.7)) << std::endl;
+    // 型を混ぜた組み合わせの計算結果表示
+    std::cout << "Min(3, 5.5f) int-float: " << compIntFloat.Min(3, 5.5f) << std::endl;
+    std::cout << "Min(3, 6.7) int-double: " << compIntDouble.Min(3, 6.7) << std::endl;
+    std::cout << "Min(5.5f, 6.7) float-double: " << compFloatDouble.Min(5.5f, 6.7) << std::endl;
 
     return 0;
 }
